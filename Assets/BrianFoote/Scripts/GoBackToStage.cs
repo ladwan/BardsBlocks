2f0b0f184e654c5f872bb51ca4c0a0cb2af6fa7f
ladwan <ladwanhester@gmail.com>
2018-07-26 14:15:36 -0400

    Fixing Scences

---
+++ Assets/BrianFoote/Scripts/GoBackToStage.cs
@@ -0,0 +1,12 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoBackToStage : MonoBehaviour {
+    public int Scenetoload;
+
+    public void GoBack()
+    {
+        SceneManager.LoadScene(Scenetoload);
+    }
+}
