65480ed25343ab9fc6074ba4fe056771decabd3d
Tranzorro <shiekadam@gmail.com>
2018-08-08 14:28:14 -0400

    rune buttons added
    ui scroll updated to include both sets of new rune buttons, one set with run icons, one without. the runeless buttons can contain additional icons on them for further animation

---
+++ Assets/AdamBrant/scripts/scrollButton.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scrollButton : MonoBehaviour
+{
+
+    Animator Scrollbottom;
+    Animator Scrollbody;
+    public GameObject fadeui;
+
+    public GameObject scrollbody;
+    public GameObject scrollbottom;
+
+    private void Start()
+    {
+        Scrollbottom = scrollbottom.GetComponent<Animator>();
+        Scrollbody = scrollbody.GetComponent<Animator>();
+        fadeui.SetActive(false);
+    }
+    public void buttonanimate()
+    {
+        if (scrollbottom != null)
+        {
+            Scrollbottom.SetTrigger("button clicked");
+            Scrollbody.SetTrigger("button clicked");
+        }
+        else
+        {
+            Debug.Log("ya fucked up");
+        }
+        
+    }
+    public void toggleActive()
+    {
+        if (fadeui.activeSelf)
+        {
+            fadeui.SetActive(false);
+        }
+        else
+        {
+            fadeui.SetActive(true);
+        }
+    }
+}
