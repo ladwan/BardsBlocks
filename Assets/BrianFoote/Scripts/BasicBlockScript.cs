84416cdba2a15e34beaf636a187feec2fa482201
ladwan <ladwanhester@gmail.com>
2018-08-09 16:35:36 -0400

    Midterm

--- Assets/BrianFoote/Scripts/BasicBlockScript.cs
+++ Assets/BrianFoote/Scripts/BasicBlockScript.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class BasicBlockScript : MonoBehaviour {
 
 	public Transform northBlock;
+    public Transform farNorthBlock;
 	public Transform eastBlock;
+    public Transform farEastBlock;
 	public Transform southBlock;
+    public Transform farSouthBlock;
 	public Transform westBlock;
+    public Transform farWestBlock;
 
 	public bool isCurrentBlock;
 	public bool destroyOnJump;
@@ -15,13 +19,23 @@ public class BasicBlockScript : MonoBehaviour {
 	public bool isAdjacentBlock;
 
 	public bool isBlockTypeBlue;
+    public Color blueGlow;
 	public bool isBlockTypeRed;
+    public Color redGlow;
 	public bool isBlockTypeYellow;
+    public Color yellowGlow;
 	public bool isBlockTypePurple;
+    public Color purpleGlow;
     public bool isBlockTypeCyan;
+    public Color cyanGlow;
 
     public float scoreValue;
     public GameObject playerRef;
+    public GameObject stageRef;
+
+    public bool isProtected;
+
+    public ParticleSystem defaultGlow;
 
 	// Use this for initialization
 	void Start () {
@@ -31,30 +45,63 @@ public class BasicBlockScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+        if (isProtected == true)
+        {
+            defaultGlow.enableEmission = true;
+        }
+        if (isProtected == false)
+        {
+            defaultGlow.enableEmission = false;
+        }
+
         playerRef = GameObject.FindGameObjectWithTag("Player");
+        stageRef = GameObject.FindGameObjectWithTag("StageReference");
 
 
-        if (destroyOnJump == true && isCurrentBlock == true) {
+        if (destroyOnJump == true && isCurrentBlock == true && isProtected == false) {
 			readyToDestroy = true;
 		}
 
-		if (readyToDestroy == true && isCurrentBlock == false) {
+		if (readyToDestroy == true && isCurrentBlock == false && isProtected == false) {
             playerRef.GetComponent<BasicPlayerScript>().playerScore += scoreValue;
+			playerRef.GetComponent<BasicPlayerScript> ().powerUpScore += scoreValue;
+            stageRef.GetComponent<GameRefScript>().stageScore += scoreValue;
 
             Destroy(gameObject);
 		}
 
+//        if (isBlockTypeBlue == true)
+//        {
+//            defaultGlow.GetComponent<ParticleSystem>().startColor = blueGlow;
+//        }
+//        if (isBlockTypeRed == true)
+//        {
+//            defaultGlow.GetComponent<ParticleSystem>().startColor = redGlow;
+//        }
+//        if (isBlockTypeYellow == true)
+//        {
+//            defaultGlow.GetComponent<ParticleSystem>().startColor = yellowGlow;
+//        }
+//        if (isBlockTypePurple == true)
+//        {
+//            defaultGlow.GetComponent<ParticleSystem>().startColor = purpleGlow;
+//        }
+//        if (isBlockTypeCyan == true)
+//        {
+//            defaultGlow.GetComponent<ParticleSystem>().startColor = cyanGlow;
+//        }
 
-//		if (Input.GetMouseButtonDown(0)) {
-//			var hit: RaycastHit;
-//			var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-//
-//			if (Physics.Raycast(ray, hit)) {
-//				if (hit.transform.name == "MyObjectName" )Debug.Log( "My object is clicked by mouse");
-//			}
-//
-//	}
+
+        //		if (Input.GetMouseButtonDown(0)) {
+        //			var hit: RaycastHit;
+        //			var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //
+        //			if (Physics.Raycast(ray, hit)) {
+        //				if (hit.transform.name == "MyObjectName" )Debug.Log( "My object is clicked by mouse");
+        //			}
+        //
+        //	}
 
 
-}
+    }
 }
--- Assets/BrianFoote/Scripts/BasicPlayerScript.cs
+++ Assets/BrianFoote/Scripts/BasicPlayerScript.cs
@@ -4,23 +4,49 @@ using UnityEngine;
 
 public class BasicPlayerScript : MonoBehaviour {
 
-    public Transform currentBlock;
+	public GameObject scoreReference;
 
-    public GameObject camHolder;
+	public Transform currentBlock;
+
+	public GameObject camHolder;
 
     public float playerScore;
-    public Animator A1,A2,B1,B3,C3;
+
+	public float powerUpScore;
+
+	public float powerUpScoreThresh;
+
+	public float doubleJumpUseCount;
+    public float doubleJumpUseMax;
+
+    public float protectUseCount;
+    public float protectUseMax;
+
+    public int powerUpPickNumber;
+
+    public bool canDoubleJump;
+    public GameObject canDoubleJumpMark;
+    public bool canProtect;
+    public GameObject canProtectMark;
+
+
+
+    public Animator A1, A2, B1, B3, C3;
     public Transform BlockA1, BlockA2, BlockB1, BlockB3, BlockC3;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
 
-        if(currentBlock == BlockA1)
+ 
+
+        // Update is called once per frame
+        void Update () {
+
+
+        if (currentBlock == BlockA1)
         {
             A1.SetTrigger("A1");
         }
@@ -40,9 +66,54 @@ public class BasicPlayerScript : MonoBehaviour {
         {
             C3.SetTrigger("C3");
         }
+
+
+        //		scoreReference = GameObject.FindGameObjectWithTag ("ScoreReference");
+
+        if (doubleJumpUseCount >= 1)
+        {
+            canDoubleJump = true;
+            canDoubleJumpMark.SetActive(true);
+        }
+
+        if (doubleJumpUseCount == 0)
+        {
+            canDoubleJump = false;
+            canDoubleJumpMark.SetActive(false);
+        }
+
+        if (doubleJumpUseCount > doubleJumpUseMax)
+        {
+            doubleJumpUseCount = doubleJumpUseMax;
+        }
+
+        if (protectUseCount >= 1)
+        {
+            canProtect = true;
+            canProtectMark.SetActive(true);
+        }
+
+        if (protectUseCount == 0)
+        {
+            canProtect = false;
+            canProtectMark.SetActive(false);
+        }
+
+        if (protectUseCount > protectUseMax)
+        {
+            protectUseCount = protectUseMax;
+        }
+//
+//        if (powerUpScore >= powerUpScoreThresh) 
+//		{
+//			powerUpScore = 0;
+//			doubleJumpUseCount += 1;
+//            protectUseCount += 1;
+//		}
+
         currentBlock.GetComponent<BasicBlockScript>().isCurrentBlock = true;
 
-		gameObject.transform.position = new Vector3 (currentBlock.position.x, 1.3f, currentBlock.position.z);
+		gameObject.transform.position = new Vector3 (currentBlock.position.x, 1.6f, currentBlock.position.z);
 
 		if (Input.GetMouseButtonDown (0)) {
 
@@ -52,20 +123,118 @@ public class BasicPlayerScript : MonoBehaviour {
 			if (Physics.Raycast (ray, out hit, 100.0f)) {
 				if (hit.transform == currentBlock.GetComponent<BasicBlockScript>().northBlock || hit.transform == currentBlock.GetComponent<BasicBlockScript>().eastBlock ||
 					hit.transform == currentBlock.GetComponent<BasicBlockScript>().southBlock || hit.transform == currentBlock.GetComponent<BasicBlockScript>().westBlock) {
-					Debug.Log (hit.transform.gameObject);
+//					Debug.Log (hit.transform.gameObject);
                     currentBlock.GetComponent<BasicBlockScript> ().isCurrentBlock = false;
 					currentBlock = hit.transform;
 					currentBlock.GetComponent<BasicBlockScript> ().isCurrentBlock = true;
+                    if (currentBlock.GetComponent<BasicBlockScript>().isProtected == true)
+                    {
+                        currentBlock.GetComponent<BasicBlockScript>().isProtected = false;
+                    }
 					JumpSpace ();
 				}
 			}
-		}
-		
-	}
+            
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            
+            if (Physics.Raycast(ray, out hit, 100.0f) && canDoubleJump == true)
+            {
+                if (hit.transform == currentBlock.GetComponent<BasicBlockScript>().farNorthBlock || hit.transform == currentBlock.GetComponent<BasicBlockScript>().farEastBlock ||
+                    hit.transform == currentBlock.GetComponent<BasicBlockScript>().farSouthBlock || hit.transform == currentBlock.GetComponent<BasicBlockScript>().farWestBlock)
+                {
+//                    Debug.Log(hit.transform.gameObject);
+                    currentBlock.GetComponent<BasicBlockScript>().isCurrentBlock = false;
+                    currentBlock = hit.transform;
+                    currentBlock.GetComponent<BasicBlockScript>().isCurrentBlock = true;
+                    if (currentBlock.GetComponent<BasicBlockScript>().isProtected == true)
+                    {
+                        currentBlock.GetComponent<BasicBlockScript>().isProtected = false;
+                    }
+                    JumpSpace();
+                    doubleJumpUseCount -= 1;
+                    //farJumpActive = false;
+                }
+
+                
+            }
+        }
+
+        if (Input.GetMouseButtonDown(2))
+        {
+
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+
+            if (Physics.Raycast(ray, out hit, 100.0f) && canProtect == true)
+            {
+                
+
+                if (hit.transform == currentBlock && canProtect == true && currentBlock.GetComponent<BasicBlockScript>().isProtected == false && currentBlock.GetComponent<BasicBlockScript>().destroyOnJump == true)
+                {
+                    currentBlock.GetComponent<BasicBlockScript>().isProtected = true;
+                    protectUseCount -= 1;
+                }
+            }
+        }
+
+    }
 
 	void JumpSpace(){
 		if (FindObjectOfType<BlockOrderScript> ().playerIsSafe == true) {
 			FindObjectOfType<BlockOrderScript> ().playerIsSafe = false;
 		}
 	}
+
+    public void RandomInt()
+    {
+        powerUpPickNumber = Random.Range(1, 3);
+        if (powerUpPickNumber == 1)
+        {
+            IncreaseDoubleJump();
+        }
+        if (powerUpPickNumber == 2)
+        {
+            IncreaseProtect();
+        }
+    }
+
+    public void IncreaseDoubleJump()
+    {
+        //doubleJumpUseCount += 1;
+        //protectUseCount += 1;
+
+        if (powerUpPickNumber == 1 && doubleJumpUseCount == doubleJumpUseMax)
+        {
+            protectUseCount += 1;
+        }
+        if (powerUpPickNumber == 1 && doubleJumpUseCount < doubleJumpUseMax)
+        {
+           doubleJumpUseCount += 1;
+        }
+        
+
+        
+    }
+    public void IncreaseProtect()
+    {
+        if (powerUpPickNumber == 2 && protectUseCount == protectUseMax)
+        {
+            doubleJumpUseCount += 1;
+        }
+        if (powerUpPickNumber == 2 && protectUseCount < protectUseMax)
+        {
+            protectUseCount += 1;
+        }
+        
+    }
+
+
 }
