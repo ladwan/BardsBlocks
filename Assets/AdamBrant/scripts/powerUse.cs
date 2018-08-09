3ce519da75eb7ee6fc9fe140849a0b96d8081e34
Tranzorro <shiekadam@gmail.com>
2018-08-09 16:45:59 -0400

    ui update, potion added
    almost got the power button to work

---
+++ Assets/AdamBrant/scripts/powerUse.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class powerUse : MonoBehaviour {
+
+    Animator power1anim;
+    Animator power2anim;
+    public GameObject power1;
+    public GameObject power2;
+    public GameObject powerIndicator;
+    // Use this for initialization
+    void Start () {
+        power1anim = power1.GetComponent<Animator>();
+        power2anim = power2.GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void slideAnim()
+    {
+        if (powerIndicator != null)
+        {
+            power1anim.SetTrigger("animate slide");
+            power2anim.SetTrigger("animate slide");
+        }
+        else
+        {
+            Debug.Log("ya fucked up");
+        }
+    }
+
+    public void toggleActive1()
+    {
+        if (power1.activeSelf)
+        {
+            power1.SetActive(false);
+        }
+        else
+        {
+            power1.SetActive(true);
+        }
+    }
+    public void toggleActive2()
+    {
+        if (power2.activeSelf)
+        {
+            power2.SetActive(false);
+        }
+        else
+        {
+            power2.SetActive(true);
+        }
+    }
+}
