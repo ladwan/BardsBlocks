e0b8e40fb8f49e34477488f86cb921c7e8831255
ladwan <ladwanhester@gmail.com>
2018-08-09 12:01:04 -0400

    lastPush

---
+++ Assets/LoadingScreenScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingScreenScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(Load());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+    IEnumerator Load()
+    {
+        yield return new WaitForSecondsRealtime(2);
+        
+
+    }
+
+}
