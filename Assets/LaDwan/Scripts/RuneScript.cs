839d20d38de94fc72ef9763d27e3384e863d339b
ladwan <ladwanhester@gmail.com>
2018-08-23 12:37:13 -0400

    RuneScriptChanges

---
+++ Assets/LaDwan/Scripts/RuneScript.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RuneScript : MonoBehaviour {
+    public GameObject Rune1, Rune2, Rune3, Rune4, Rune5;
+    int CurrentStage = 1, RunesCollected = 0;
+    GameObject NextNoteREF;
+    Transform CurrentBlockREF;
+    public GameObject WaterRune, FireRune, AirRune;
+
+    
+	// Use this for initialization
+	void Start () {
+        if (CurrentStage == 1)
+        {
+            Rune3.SetActive(false);
+            Rune4.SetActive(false);
+
+            Rune1.GetComponent<Button>().enabled = false;
+            Rune2.GetComponent<Button>().enabled = false;
+            Rune5.GetComponent<Button>().enabled = false;
+
+
+        }
+        StartCoroutine(SongOrder());
+    }
+	
+    IEnumerator SongOrder()
+    {
+        yield return new WaitForSeconds(2);
+        Rune5.GetComponent<Button>().enabled = true;
+        yield return new WaitForSeconds(1);
+        Rune2.GetComponent<Button>().enabled = true;
+        Rune5.GetComponent<Button>().enabled = false;
+        yield return new WaitForSeconds(1);
+        Rune1.GetComponent<Button>().enabled = true;
+        Rune2.GetComponent<Button>().enabled = false;
+        yield return new WaitForSeconds(1);
+        Rune1.GetComponent<Button>().enabled = false;
+
+
+
+
+    }
+    // Update is called once per frame
+    void Update () {
+
+
+            if (RunesCollected == 0 && gameObject.transform != WaterRune.transform)
+            {
+                Rune5.GetComponent<Button>().enabled = true;
+                RunesCollected++;
+            }
+
+
+
+            if (RunesCollected == 1 && gameObject.transform == FireRune.transform)
+            {
+                Rune5.GetComponent<Button>().enabled = true;
+                RunesCollected++;
+            }
+
+            if (RunesCollected == 2 && gameObject.transform == AirRune.transform)
+            {
+                Rune5.GetComponent<Button>().enabled = true;
+                RunesCollected = 0;
+                CurrentStage = 2;
+            }
+        
+
+    }
+}
