ba9eed97ef1d75e26b02b2ba1b8e17d6bee8e9ec
ladwan <ladwanhester@gmail.com>
2018-09-20 12:48:51 -0400

    Scene Tips

--- Assets/LaDwan/Scripts/LoadingScreenScript.cs
+++ Assets/LaDwan/Scripts/LoadingScreenScript.cs
@@ -5,7 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class LoadingScreenScript : MonoBehaviour {
     public int scencetoload;
-
+    
+    
+    
 	// Use this for initialization
 	void Start () {
         StartCoroutine(Load());
@@ -13,12 +15,18 @@ public class LoadingScreenScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        scencetoload = SceneREF.CurrentScene;
 
-
+       
 	}
     IEnumerator Load()
     {
-        yield return new WaitForSecondsRealtime(4);
+        scencetoload = SceneREF.CurrentScene;
+        if (scencetoload == 2)
+        {
+            scencetoload = 6;
+        }
+        yield return new WaitForSecondsRealtime(5);
         SceneManager.LoadScene(scencetoload);
 
     }
